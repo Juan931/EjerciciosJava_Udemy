16989f482e83a1bf302534c5652e0f992cac993d
Juan medina <juan@DESKTOP-DFEEQ9C.localdomain>
2021-09-28 15:57:41 -0500

    Operaciones Racionales

--- src/udemy/poo/codigo/OperacionesRacionales.java
+++ src/udemy/poo/codigo/OperacionesRacionales.java
@@ -19,6 +19,36 @@ public class OperacionesRacionales {
         this.racionalDos = racionalDos;
     }
     
-    
+    public Racional suma(){
+        int numerador = (this.racionalUno.getNumerador() * this.racionalDos.getDenominador())
+                + (this.racionalUno.getDenominador() *this.racionalDos.getNumerador());
+        int denomindor   = this.racionalUno.getDenominador() *this.racionalDos.getDenominador();
+        return new Racional(numerador, denomindor);
+    }
+     public Racional resta(){
+        int numerador = (this.racionalUno.getNumerador() * this.racionalDos.getDenominador())
+                - (this.racionalUno.getDenominador() *this.racionalDos.getNumerador());
+        int denomindor   = this.racionalUno.getDenominador() *this.racionalDos.getDenominador();
+        return new Racional(numerador, denomindor);
+    }
+     public Racional multiplicacion(){
+         int numerador = this.racionalUno.getNumerador() * this.racionalDos.getNumerador();
+         int denominador = this.racionalDos.getDenominador() * this.racionalDos.getDenominador();
+         return new Racional(numerador, denominador);
+     }
+            
+     public Racional division (){
+         int numerador = this.racionalUno.getNumerador() * this.racionalDos.getDenominador();
+         int denominador = this.racionalUno.getDenominador() * this.racionalDos.getNumerador();
+         return new Racional(numerador, denominador);
+     }
+
+    public Racional getRacionalUno() {
+        return racionalUno;
+    }
+
+    public Racional getRacionalDos() {
+        return racionalDos;
+    }
     
 }
