861f3e9adf87df77eab55ab110a914736c4555e8
Juan Medina <juandavidds08@gmail.com>
2021-09-30 19:01:52 -0500

    implementacion de Interface

--- src/udemy/poo/inicio/Inicio.java
+++ src/udemy/poo/inicio/Inicio.java
@@ -8,6 +8,7 @@ package udemy.poo.inicio;
 import javax.swing.JOptionPane;
 import udemy.poo.codigo.Elipse;
 import udemy.poo.codigo.ElipseDos;
+import udemy.poo.codigo.ElipseImplements;
 import udemy.poo.codigo.Empleado;
 import udemy.poo.codigo.JefeDeArea;
 import udemy.poo.codigo.Rectangulo;
@@ -20,6 +21,7 @@ public class Inicio {
     public static void main(String[] args) {
         Elipse elp = new Elipse(3, 2);
         ElipseDos elpDos = new ElipseDos(3, 2, 3, 2);
+        ElipseImplements elpImp = new ElipseImplements(3, 2);
         Rectangulo rec = new Rectangulo(4, 4);
         Empleado emp = new Empleado("JhoN ", 10000);
         JefeDeArea jef = new JefeDeArea("Juan", 20000);
@@ -29,6 +31,7 @@ public class Inicio {
         JOptionPane.showMessageDialog(null, emp.incrementoSueldo());
         JOptionPane.showMessageDialog(null, jef.incrementoSueldo());*/
         JOptionPane.showMessageDialog(null,"Area de Elipse (abstract)" + elp.area() + "\n" 
-                +"Area de Elipse (Normal)" + elpDos.area());
+                +"Area de Elipse (Normal)" + elpDos.area() + "\n"
+                +"Area de Elipse (Interface)" + elpImp.area() + "\n" );
     }
 }
